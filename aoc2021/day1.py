247c680e5de65dd457959e9af043faddf471dda8
Benjamin Lyng Jørgensen <benjamin@lyng.space>
2021-12-01 08:26:18 +0100

    day1 part2

--- aoc2021/day1.py
+++ aoc2021/day1.py
@@ -1,6 +1,9 @@
-from more_itertools import pairwise
+from more_itertools import pairwise, triplewise
 
 with open("inputs/day1.txt") as f:
     depths = [int(x) for x in f.readlines()]
 
-print(len([a - b for a, b in pairwise(depths) if a<b]))
+print(len([True for a, b in pairwise(depths) if a<b]))
+
+bins = [a+b+c for a,b,c in triplewise(depths)]
+print(len([True for a, b in pairwise(bins) if a<b]))
